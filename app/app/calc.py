c9f6dc62e795171d98d0bc4e0c7252f0a5f46bc4
redihoxha <redi.hoxha@y42.com>
2023-06-23 16:15:38 +0200

    Format sample tests for Lint.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,8 @@
 """
 Calculator function
 """
-
-
 def add(x, y):
     return x + y
 
-
 def subtract(x, y):
-
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,19 +2,15 @@
 Sample tests
 """
 from django.test import SimpleTestCase 
-
 from app import calc
-
 
 class CalcTest(SimpleTestCase):
     """Test the calc module"""
-
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
 
