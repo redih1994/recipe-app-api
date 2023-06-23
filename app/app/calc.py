c54654f1fafb0d2a9c1d7836eb64617846ea3ade
redihoxha <redi.hoxha@y42.com>
2023-06-23 16:37:38 +0200

    Re-format.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,8 +1,11 @@
 """
 Calculator function
 """
+
+
 def add(x, y):
     return x + y
+
 
 def subtract(x, y):
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,14 @@
 """
 Sample tests
 """
-from django.test import SimpleTestCase 
+from django.test import SimpleTestCase
+
 from app import calc
+
 
 class CalcTest(SimpleTestCase):
     """Test the calc module"""
+
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
@@ -13,7 +16,6 @@ class CalcTest(SimpleTestCase):
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
