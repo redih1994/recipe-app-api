fc18dc451c49708291cb545e9b3056c35126ef49
redihoxha <redi.hoxha@y42.com>
2023-06-24 20:25:47 +0200

    Create the database model and the custom model for the user. Add the tests for the user creation and admin.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,46 @@
+"""
+Tests for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new user"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'samplepassword')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test if the user has entered the email address"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'samplepass')
+
+    def test_create_superuser(self):
+        """Test the creation of the superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'samplepass'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
